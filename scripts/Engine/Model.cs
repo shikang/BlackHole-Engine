a5f2e11ea0322fa3acf7c462c042f040b115df68
kts-shikang <shikang.neoh@koeitecmo.com.sg>
2016-07-05 17:43:52 +0800

    Animation Controller with scripts

--- scripts/Engine/Model.cs
+++ scripts/Engine/Model.cs
@@ -6,10 +6,21 @@ namespace BH
     public class BHModel
     {
         public string Model = "";
+        private int AnimationController = -1;
 
         public BHModel( string model )
         {
             Model = model;
+        }
+
+        public void InitialiseModel()
+        {
+            AnimationController = CPlusPlusInterface.CreateAnimationController( Model );
+        }
+
+        public void ShutdownModel()
+        {
+            CPlusPlusInterface.DeleteAnimationController( AnimationController );
         }
     }
 }
